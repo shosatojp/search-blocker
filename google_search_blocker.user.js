737ba0e78cd27c75ae68b094abb84a03a6f219c4
佐藤翔 <shosatojp2001@outlook.jp>
2019-01-15 17:14:49 +0900

    -

--- google_search_blocker.user.js
+++ google_search_blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Search Blocker
 // @namespace    https://github.com/ShoSatoJp/
-// @version      0.9.24
+// @version      0.9.25
 // @description  block undesired sites from google search results!
 // @author       ShoSato
 // @match https://www.google.co.jp/search?*
