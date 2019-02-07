cb5220f07a7a6f9ad58a2adcc04d05a42aa04c17
佐藤翔 <shosatojp2001@outlook.jp>
2019-02-07 09:58:44 +0900

    0.10.16

--- google_search_blocker.user.js
+++ google_search_blocker.user.js
@@ -2,7 +2,7 @@
 // @name         Google Search Blocker
 // @namespace    https://github.com/shosatojp/google_search_blocker
 // @homepage https://github.com/shosatojp/google_search_blocker
-// @version      0.10.15
+// @version      0.10.16
 // @description  block undesired sites from google search results!
 // @author       Sho Sato
 // @match https://www.google.co.jp/search?*
