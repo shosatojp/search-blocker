64d8769e12538298598620278fdce422dc837867
佐藤翔 <shosatojp2001@outlook.jp>
2019-02-15 11:10:23 +0900

    0.13.4

--- google_search_blocker.user.js
+++ google_search_blocker.user.js
@@ -2,7 +2,7 @@
 // @name         Google Search Blocker
 // @namespace    https://github.com/shosatojp/google_search_blocker/raw/master/google_search_blocker.user.js
 // @homepage     https://github.com/shosatojp/google_search_blocker
-// @version      0.13.3
+// @version      0.13.4
 // @description  Block undesired sites from google search results!
 // @author       Sho Sato
 // @match        https://www.google.com/search?*
@@ -1027,7 +1027,10 @@
             }, function onsignout() {
                 R.signin.style.display = 'block';
                 R.signout.style.display = 'none';
-            })).initSync().then(() => SYNC.compare()).catch(() => {});
+            })).initSync().then(() => SYNC.compare()).catch(() => {
+                R.syncinfo.textContent = LOCAL_STRING.failedtosync;
+                SYNC.setUseSync(false);
+            });
 
         });
     }
